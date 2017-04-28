74370285412fdc72e63ef344323acd0699050fe9
Nick <MykolaKoval91@gmail.com>
2017-04-28 17:55:57 +0300

    add some files to gitignore

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module Kmblog
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    #config.time_zone = 'Kyiv'
+    config.time_zone = 'Kyiv'
     config.i18n.available_locales = [:uk, :en]
     config.i18n.default_locale = :uk
   end
