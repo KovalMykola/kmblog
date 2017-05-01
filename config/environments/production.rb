09623ee4aa54d1afa19cf13c075ff72b00d81385
Nick <MykolaKoval91@gmail.com>
2017-05-01 21:24:34 +0300

    add default url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,7 +84,21 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  # Rails.application.routes.default_url_options[:host] = 'https://morning-coast-45370.herokuapp.com'
+
+  #config.action_controller.asset_host = 'https://morning-coast-45370.herokuapp.com'
+  #config.action_mailer.default_url_options = { :host => 'https://morning-coast-45370.herokuapp.com' }
+  #config.action_mailer.asset_host = 'https://morning-coast-45370.herokuapp.com/'
+
+  Rails.application.routes.default_url_options[:host] = 'https://morning-coast-45370.herokuapp.com'
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
+
+
+
   #config.assets.digest = true
 
-  config.action_mailer.default_url_options = { :host => 'https://morning-coast-45370.herokuapp.com' }
+  # config.action_mailer.default_url_options = { :host => 'https://morning-coast-45370.herokuapp.com' }
 end
