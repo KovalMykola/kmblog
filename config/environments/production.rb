d741d21a35837e6fc9f5afdb804f89d225626ed2
Nick <MykolaKoval91@gmail.com>
2017-05-01 16:39:15 +0300

    deploy v3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,4 +85,6 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.assets.digest = true
+
+  config.action_mailer.default_url_options = { :host => 'https://morning-coast-45370.herokuapp.com' }
 end
